a448d76963712ec45c3b60d3d840554281f656de
AREUM AN <areum.an.0928@gmail.com>
2018-10-07 04:29:44 -0400

    state set up 1

--- todo-list/src/MyComponent.js
+++ todo-list/src/MyComponent.js
@@ -3,6 +3,17 @@ import PropTypes from 'prop-types';
 
 
 class MyComponent extends Component {
+    constructor(props) {
+        super(props);
+        /* state set 1 */
+        // this.state = {
+        //     number: 0
+        // }
+    }
+    /* state set 2 */
+    state = {
+        number: 0
+    }
     /* default props set up 2 */
     static defaultProps = {
         name: 'Default Name'
@@ -17,10 +28,17 @@ class MyComponent extends Component {
             <div>
                 Hi, my name is {this.props.name}.<br />
                 I am {this.props.age}.
+                <p>Number: {this.state.number}</p>
+                <button onClick={() => {
+                    this.setState({
+                        number: this.state.number + 1
+                    })
+                }}>PLUS</button>
             </div>
         );
     }
 }
+
 
 /* default props set up 1 */
 // MyComponent.defaultProps = {
