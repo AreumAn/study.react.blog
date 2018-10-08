5b1517d2258e272bf0c16fae0dac1606915b1016
AREUM AN <areum.an.0928@gmail.com>
2018-10-08 07:11:51 -0400

    How to use ref

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import MyComponent from './MyComponent';
 import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <ScrollBox ref={(ref) => this.scrollBox=ref}/>
+        <button onClick={() => this.scrollBox.scrollToBottom()}>Go to Bottom</button>
+        <hr />
+        <ValidationSample />
+        <hr />
         <EventPractice/>
         <hr />
         <MyComponent name='react' age={4}/>
