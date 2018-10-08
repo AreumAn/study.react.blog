5971364340307587fe532463befa0040cd8519af
AREUM AN <areum.an.0928@gmail.com>
2018-10-08 08:38:49 -0400

    State Remove with filter

--- todo-list/src/IterationSample.js
+++ todo-list/src/IterationSample.js
@@ -24,10 +24,13 @@ class IterationSample extends Component {
         const { names } = this.state;
 
         this.setState({
-            names: [
-                ...names.slice(0, index),
-                ...names.slice(index + 1, names.length)
-            ]
+            names: names.filter((item, i) => i !== index)
+
+            /* map */
+            // names: [
+            //     ...names.slice(0, index),
+            //     ...names.slice(index + 1, names.length)
+            // ]
         })
     }
 
