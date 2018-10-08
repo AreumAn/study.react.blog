9eb5dd68df1c8b29d2c52ba230fadba40f936230
AREUM AN <areum.an.0928@gmail.com>
2018-10-08 18:32:01 -0400

    LifeCycle

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -5,11 +5,29 @@ import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
 import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    })
+  }
   render() {
     return (
       <div>
+        <button onClick={this.handleClick}>Random Color</button>
+        <LifeCycleSample color={this.state.color} />
+        <hr />
         <IterationSample />
         <hr />
         <ScrollBox ref={(ref) => this.scrollBox=ref}/>
