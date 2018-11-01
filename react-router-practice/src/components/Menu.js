16e3429954cdf3c647129f8b7ae0252240bf9cfd
AREUM AN <areum.an.0928@gmail.com>
2018-11-01 11:59:42 -0400

    route NavLink

--- react-router-practice/src/components/Menu.js
+++ react-router-practice/src/components/Menu.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Menu = () => {
+
+    const activeStyle = {
+        color: 'green',
+        fontSize: '2rem'
+    };
+
     return (
         <div>
             <ul>
-                <li><Link to = "/">Home</Link></li>
-                <li><Link to = "/about">About</Link></li>
-                <li><Link to = "/about/react">Intro React</Link></li>
+                <li><NavLink exact to = "/" activeStyle={activeStyle}>Home</NavLink></li>
+                <li><NavLink exact to = "/about" activeStyle={activeStyle}>About</NavLink></li>
+                <li><NavLink to = "/about/react" activeStyle={activeStyle}>Intro React</NavLink></li>
             </ul>
         </div>
     )
