97287ea7e2eea9588bf0a2f8e2b056649697ea7c
AREUM AN <areum.an.0928@gmail.com>
2018-11-01 11:46:53 -0400

    router query-string

--- react-router-practice/src/pages/About.js
+++ react-router-practice/src/pages/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import queryString from 'query-string';
 
-const About = ({match}) => {
+const About = ({location, match}) => {
+    const query = queryString.parse(location.search);
+    
+    const { color } = query;
+
     return (
         <div>
-            <h2>About</h2>
+            <h2 style={{color}}>About</h2>
             <p>Hello, I am { match.params.name }!!!!</p>
         </div>
     )
