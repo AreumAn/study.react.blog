6473861fec7391bc6cb8f379197ff05dea4d2e3f
AREUM AN <areum.an.0928@gmail.com>
2018-11-02 01:10:01 -0400

    set env

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,8 +1,15 @@
+require('dotenv').config();
+
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 
 const api = require('./api');
+
+const {
+    PORT: port = 4000,
+    MONGO_URI: mongoURI
+  } = process.env;
 
 const app = new Koa();
 const router = new Router();
@@ -13,6 +20,6 @@ app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-    console.log('listening to port 4000');
+app.listen(port, () => {
+    console.log('listening to port', port);
 });
