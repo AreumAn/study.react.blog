909971278efc8037112ece02ba1b55eff531d895
AREUM AN <areum.an.0928@gmail.com>
2018-11-02 13:19:58 -0400

    Valid requst ObjectId

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,4 +1,17 @@
+const { ObjectId } = require('mongoose').Types;
+
 const Post = require('../../models/post');
+
+exports.checkObjectId = (ctx, next) => {
+    const { id } = ctx.params;
+
+    if (!ObjectId.isValid(id)) {
+      ctx.status = 400;
+      return null;
+    }
+
+    return next();
+  };
 
 /*
     Write Post
