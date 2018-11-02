80e1878adf2c2cea90015f6c0d9042c039531a03
AREUM AN <areum.an.0928@gmail.com>
2018-11-01 20:33:56 -0400

    postman

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,23 +1,12 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 
+const api = require('./api');
+
 const app = new Koa();
 const router = new Router();
 
-// Set router
-router.get('/', (ctx) => {
-    ctx.body = 'Home';
-});
-
-router.get('/about/:name?', (ctx) => {
-    const { name } = ctx.params;
-    ctx.body = name ? `About ${name}` : 'About';
-});
-
-router.get('/posts', (ctx) => {
-    const { id } = ctx.query;
-    ctx.body = id ? `POST  ##${id}` : 'No Post ID';
-});
+router.use('/api', api.routes());
 
 app.use(router.routes()).use(router.allowedMethods());
 
