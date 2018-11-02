56199ae3c15762ac7604484b6a41d5119b17054f
AREUM AN <areum.an.0928@gmail.com>
2018-11-02 13:58:43 -0400

    pagenation with toJSON

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -57,8 +57,27 @@ exports.write = async (ctx) => {
 */
 
 exports.list = async (ctx) => {
+    const page = parseInt(ctx.query.page || 1, 10);
+
+    if(page < 1) {
+        ctx.status = 400;
+        return;
+    }
+
     try {
-        const posts = await Post.find().exec();
+        const posts = await Post.find()
+            .sort({ _id: -1 })
+            .limit(10)
+            .skip((page - 1) * 10)
+            .exec();
+        const postCount = await Post.count().exec();
+
+        const limitBodyLength = post => ({
+            ...post.toJSON(),
+            body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`
+        });
+
+        ctx.set('Last-Page', Math.ceil(postCount / 10));
         ctx.body = posts;
     } catch (e) {
         ctx.throw(e, 500);
