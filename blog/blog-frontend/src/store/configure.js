bf6543dac0537fce7c492d44b0ae07fd0646084d
AREUM AN <areum.an.0928@gmail.com>
2018-11-03 00:15:23 -0400

    Set redux

---
+++ blog/blog-frontend/src/store/configure.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import penderMiddleware from "redux-pender";
+import * as modules from './modules';
+
+const reducers = combineReducers(modules);
+const middlewares = [penderMiddleware()];
+
+// Only Dev mode -> use Redux Devtools
+const isDev = process.env.NODE_ENV === 'development';
+const devtools = isDev && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = devtools || compose;
+
+const configure = (preloadedState) => createStore(reducers, preloadedState, composeEnhancers(
+    applyMiddleware(...middlewares)
+  ));
+  
+export default configure;
