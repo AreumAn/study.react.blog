9ea388e8ac452c0616e9e75e4f3bdcae25fe5ebb
AREUM AN <areum.an.0928@gmail.com>
2018-11-03 21:33:12 -0400

    Footer

--- blog/blog-frontend/src/components/common/Footer/Footer.js
+++ blog/blog-frontend/src/components/common/Footer/Footer.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './Footer.scss';
 import classNames from 'classnames/bind'
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 const Footer = () => (
-    <div className={cx('page-template')}>
-        Footer
-    </div>
+    <footer className={cx('footer')}>
+        <Link to="/" className={cx('brand')}>Areum's Blog</Link>
+        <div className={cx('admin-login')}>Login as admin</div>
+    </footer>
 );
 
 
