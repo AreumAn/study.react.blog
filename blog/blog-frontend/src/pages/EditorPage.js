2a0c822e52785e158a4508fd35f32173b7b560b9
AREUM AN <areum.an.0928@gmail.com>
2018-11-04 19:15:25 -0500

    EditHeader

--- blog/blog-frontend/src/pages/EditorPage.js
+++ blog/blog-frontend/src/pages/EditorPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import EditorTemplate from '../components/editor/EditorTemplate/';
+import EditorHeader from '../components/editor/EditorHeader/EditorHeader';
 
 const EditorPage = () => {
     return (
         <EditorTemplate 
-            header="Header"
+            header={<EditorHeader />}
             editor="Editor"
             preview="Preview"
         />
